f5ce137302760baa1ba72749615c73a41f19a758
januarjafar <januar.jafar@gmail.com>
2025-06-25 12:34:40 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Halo Tauke 🚀</Text>
+      <Text>App ini siap bantu kontrol pinjamanmu!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f1f1f1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
